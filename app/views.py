08408493169d0bb7e9162e8c215e9847e5cf87d5
kLeitich <kevleitich@gmail.com>
2022-08-06 15:49:55 +0300

    adding html for login,signup,start and base

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
+import re
 from django.shortcuts import render
 
 # Create your views here.
 def home(request):
     return render(request,'index.html')
+
+def start(request):
+    return render(request,"authenticate/start.html")
+
