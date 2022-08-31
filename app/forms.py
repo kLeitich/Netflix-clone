8ce249ca9e2f2feb3b4a24098617e956b153d42e
kLeitich <kevleitich@gmail.com>
2022-09-01 01:11:08 +0300

    styling signing up page with step one almost done

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,24 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegistrationForm(UserCreationForm):
-   
+    def __init__(self, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.fields['email'].widget.attrs.update({
+        "required":"",
+        "name":"username",
+        "id":"name",
+        "type":"text",
+        "placeholder":"Email",
+        "class":"form-input"
+      })
+      self.fields['password1'].widget.attrs.update({
+        "required":"",
+        "name":"password1",
+        "id":"password1",
+        "type":"password",
+        "placeholder":"Password",
+        "class":"form-input"
+      })
 
     class Meta:
         model = User
